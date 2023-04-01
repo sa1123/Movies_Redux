d7c1231a38034c489580ad48aad8c13ced0f251a
Sahil <51375919+sa1123@users.noreply.github.com>
2023-04-01 20:29:22 +0530

    favourites

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,22 +1,38 @@
 import React from "react";
+import { addFavourite, removeFromFavourites } from "../actions";
 
 class MovieCard extends React.Component {
-  render () {
-    const {movie} = this.props;
-    return (
-        <div className="movie-card">
-            <div className="left">
-                <img alt="movie-poster" src={movie.Poster}/>
-            </div>
-            <div className="right">
-                <div className="title">{movie.Title}</div>
-                <div className="plot">{movie.Plot}</div>
-                <div className="footer">
-                    <div className="rating">{movie.imdbRating}</div>
-                    <button className="favourite-btn">Favourite</button>
+
+    handleFavouriteClick = () => {
+        const {movie} = this.props;
+        this.props.dispatch(addFavourite(movie))
+    }
+
+    handleUnFavouriteClick = () => {
+        const {movie} = this.props;
+        this.props.dispatch(removeFromFavourites(movie))
+    }
+
+    render () {
+        const {movie, isFavourite} = this.props;
+        return (
+            <div className="movie-card">
+                <div className="left">
+                    <img alt="movie-poster" src={movie.Poster}/>
+                </div>
+                <div className="right">
+                    <div className="title">{movie.Title}</div>
+                    <div className="plot">{movie.Plot}</div>
+                    <div className="footer">
+                        <div className="rating">{movie.imdbRating}</div>
+                        {
+                            isFavourite
+                            ? <button className="unfavourite-btn" onClick={this.handleUnFavouriteClick}>Unfavourite</button>
+                            : <button className="favourite-btn" onClick={this.handleFavouriteClick}>Favourite</button>
+                        }
+                    </div>
                 </div>
             </div>
-        </div>
         );
     }
 }
